27f649acea5ba11516775d48f38dab1394c9bc13
lamjarad-B <lamjarad@gmail.com>
2024-11-05 12:53:40 +0100

    Les composant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { POKEMONS } from './mock-pokemon-list'; // Import de la liste de Pokémons mockée
+import { Pokemon } from './pokemon'; // Import du modèle de données d'un Pokémon
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  templateUrl: 'app.component.html' // pas besoin d'un chemin relatif puisque les deux fichiers se trouvent dans le même dossier
 })
-export class AppComponent {
-  title = 'pokemon-app';
+export class AppComponent implements OnInit{
+  // Déclaration et initialisation de la liste des Pokémons depuis la liste mockée
+  // pokemonList est un tableau de Pokemon
+  pokemonList:Pokemon[] = POKEMONS;
+
+  ngOnInit(): void {
+      console.table(this.pokemonList);// this pour dire la proprièté pokemonList de cet objet
+
+      this.selectPokemon(this.pokemonList[3]);
+  }
+
+  selectPokemon(pokemon: Pokemon){
+    console.log(`Vous avez cliqué sur le pokemon ${pokemon.name}`);
+  }
 }
---
+++ src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [],
+  bootstrap: []
+})
+export class AppModule { }
